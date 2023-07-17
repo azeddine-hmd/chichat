545da552461fcfa434f1224cfda7572750deb189
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2023-07-17 22:52:03 +0100

    docker compose services added and automating start up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,39 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { randomInt } from 'crypto';
+import bodyParser from 'body-parser';
+import createHttpError from 'http-errors';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
+const env = process.env;
 
-const app = express();
-const port = process.env.PORT;
+export const app = express();
 
-console.log(`port is ${port}`);
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Express + Typescript Server');
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  console.log('Time:', Date.now().toLocaleString());
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.post('/users', (req, res, next) => {
+  console.log(`request body: ${JSON.stringify(req.body)}`);
+  if (randomInt(10) < 5) {
+    next(createHttpError(400, 'failed to register user!'));
+  }
+  res.send('user added successfully');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send('<h1>Internal Server Error</h1>');
+});
+
+app.listen(env.PORT, () => {
+  console.log(
+    `[server]: Server is running at ${env.PROTOCOL}://${env.HOST}:${env.PORT}`
+  );
 });
