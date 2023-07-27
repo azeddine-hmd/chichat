db8669bc247b1c828a5705a09e139c5cc9692d0c
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2023-07-27 09:59:24 +0100

    moving to nodemailer for sending mails

--- backend/src/utils/send-mail.ts
+++ backend/src/utils/send-mail.ts
@@ -1,14 +1,35 @@
-import { mg } from '../config';
+// import { mg } from '../config';
+import nodemailer from 'nodemailer';
 
 export async function sendVerificationMail(to: string, htmlContent: string) {
-  return await mg.messages.create(
-    'sandboxebcb4c88e0454f0181e28e38dda7e503.mailgun.org',
-    {
-      from: 'Discord  <mailgun@sandboxebcb4c88e0454f0181e28e38dda7e503.mailgun.org>',
+  // return await mg.messages.create(
+  //   'sandboxebcb4c88e0454f0181e28e38dda7e503.mailgun.org',
+  //   {
+  //     from: 'Discord  <mailgun@sandboxebcb4c88e0454f0181e28e38dda7e503.mailgun.org>',
+  //     to: [to],
+  //     subject: 'Email Validation',
+  //     text: 'Testing some Mailgun awesomeness!',
+  //     html: htmlContent,
+  //   }
+  // );
+  
+  // send via nodemail
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    auth: {
+      user: 'azeddine.hmd@gmail.com',
+      pass: 'InnocentDevil_666',
+    },
+    secure: true,
+    authMethod: 'PLAIN',
+  })
+
+  await transporter.sendMail({
+      from: 'Azeddine Hamdaoui  <smtp.gmail.com>',
       to: [to],
       subject: 'Email Validation',
-      text: 'Testing some Mailgun awesomeness!',
+      text: 'This is just for development application!',
       html: htmlContent,
-    }
-  );
+  })
 }
