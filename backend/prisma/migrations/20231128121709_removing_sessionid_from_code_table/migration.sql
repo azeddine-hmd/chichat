5c0f45caa14e543be2fb92a67ec0ef490a001a1e
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2023-11-28 13:19:36 +0100

    removing sessionid from code table

---
+++ backend/prisma/migrations/20231128121709_removing_sessionid_from_code_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `sessionId` on the `Codes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Codes" DROP COLUMN "sessionId";
