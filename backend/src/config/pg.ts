34692fb95a58ecf7e2780c18843d366630518021
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2023-12-09 08:38:49 +0100

    configuring postgresql adapter with socket io

---
+++ backend/src/config/pg.ts
@@ -0,0 +1,22 @@
+import { Pool } from 'pg';
+
+console.log('loading pg.ts file');
+
+export const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
+pool.query(`
+  CREATE TABLE IF NOT EXISTS socket_io_attachments (
+    id          bigserial   UNIQUE,
+    created_at  timestamptz DEFAULT NOW(),
+    payload     bytea
+  );
+`);
+
+pool.on('error', (err) => {
+  console.error('Postgres error', err);
+});
