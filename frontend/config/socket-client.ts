5ed523e9a7efbe24208d9950bd26ff4eb4f88c24
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2023-12-27 07:39:02 +0100

    small change to socket connection in client side

--- frontend/config/socket-client.ts
+++ frontend/config/socket-client.ts
@@ -1,30 +1,26 @@
 import { io } from "socket.io-client";
 
 export type ConnectSocket = {
-  onReady: () => void
-  onDisconnect: () => void
+  onReady: () => void;
+  onDisconnect: () => void;
 };
 
 export function connectSocket({ onReady, onDisconnect }: ConnectSocket) {
   return new Promise((resolve) => {
-    if (!window.clientSocket?.connected) {
-      window.clientSocket = io(process.env.NEXT_PUBLIC_BACKEND_DOMAIN as string, {
-        transports: ["websocket"],
-        withCredentials: true,
+    window.clientSocket = io(process.env.NEXT_PUBLIC_BACKEND_DOMAIN as string, {
+      transports: ["websocket"],
+      withCredentials: true,
+    });
+    window.clientSocket.on("connect", () => {
+      window.clientSocket.once("ready", (...args) => {
+        console.log(`server socket ready at ${args[0]}s`);
+        onReady();
+        resolve(window.clientSocket);
       });
-      window.clientSocket.on('connect', () => {
-        window.clientSocket.once('ready', (...args) => {
-          // console.log(`server socket ready at ${args[0]}s`);
-          onReady();
-          resolve(window.clientSocket);
-        })
-        // setTimeout(() => resolve(window.clientSocket), 5_000);
-      });
-      window.clientSocket.on('disconnect', () => {
-        onDisconnect();
-      })
-    } else {
-      resolve(window.clientSocket);
-    }
+      // setTimeout(() => resolve(window.clientSocket), 5_000);
+    });
+    window.clientSocket.on("disconnect", () => {
+      onDisconnect();
+    });
   });
 }
